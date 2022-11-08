405721437a9e6b41a5fa4f5e663cad27aeea2ede
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-11-08 16:45:19 +0900

    02

--- main.c
+++ main.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 int main(void) {
-   char code1='A';
-   int code2= 65;
+   int i=0;
+   char str[4];
+   str[0]='a';
+   str[1]='b';
+   str[2]='c';
+   str[3]='\0';
    
-   printf("code1=%c,(%d)\n",code1,code1);
-   printf("code2=%c,(%d)\n",code2,code2);
+   printf("%s\n",str);
    
+   while(str[i]!='\0')
+   {
+      printf("str[%d]=%c\n",i,str[i]);
+      i++;
+   }
+
 }

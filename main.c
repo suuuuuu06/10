ea0cdac69b8ead3694a5c0067751302e10aaf27d
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-11-08 16:52:35 +0900

    04

--- main.c
+++ main.c
@@ -3,11 +3,10 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 #include<string.h>
+
 void main(void){
-   char src[]="The worst things to eat before you sleep";
-   char dst[100];
+   char str[30]="happy C programming";
+    
    
-   strcpy(dst,src);
-   
-   printf("copied string:%s",dst);
+   printf("문자열\"%s\"의 길이: %i",str,strlen(str));//<문자열 길이>=strlen(<입력 문자열>));
 }

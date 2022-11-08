709895881f0c9183029bbfd0ad41995ad53814a9
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-11-08 16:51:30 +0900

    03

--- main.c
+++ main.c
@@ -2,21 +2,12 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void) {
-   int i=0;
-   char str[4];
-   str[0]='a';
-   str[1]='b';
-   str[2]='c';
-   str[3]='\0';
+#include<string.h>
+void main(void){
+   char src[]="The worst things to eat before you sleep";
+   char dst[100];
    
-   printf("%s\n",str);
+   strcpy(dst,src);
    
-   while(str[i]!='\0')
-   {
-      printf("str[%d]=%c\n",i,str[i]);
-      i++;
-   }
-
+   printf("copied string:%s",dst);
 }
